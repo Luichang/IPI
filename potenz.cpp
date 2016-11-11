5401f48ed2909bcb9be870d68b2cd2424f37bc48
Luichang <cbarbret@bucs.fsw.edu>
2016-11-11 12:20:08 +0100

    pretend if neu verlegt

--- potenz.cpp
+++ potenz.cpp
@@ -6,17 +6,14 @@ double sq(double x){
 
 double repeatTwice(double x, int n){
 	double y;
-	std::cout << "to test x = " << x << " n = " << n << std::endl;
-	//(n % 2 == 0) ? y = sq(x), n = n/2 : (n == 1) ? y = x : y = x * x, n = n - 1;
-	y = sq(x), n = n/2;
-	std::cout << "y = " << y << std::endl;
+	(n % 2 == 0) ? y = sq(x) : (n == 1) ? y = x : y = x * x;
 	return y;
 }
 
 double power(double x, int n){
 	double y;
 	int m;
-	y = repeatTwice(x, n);
+	(n % 2 == 0) ? y = repeatTwice(x, n), n = n/2 : (n == 1) ? y = x : y = repeatTwice(x, n), n = n - 1;
 	std::cout << "to test x = " << x << " y = " << y << " and m = " << m << std::endl;
 	(n != 1) ? y = repeatTwice(y, m) : y = y;
 	return y;
