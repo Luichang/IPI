e477b908e12573cc9dfdbbd2bb7f2f8368338ad9
Luichang <cbarbret@bucs.fsw.edu>
2016-11-12 01:21:19 +0100

    Works for n > 5

--- potenz.cpp
+++ potenz.cpp
@@ -1,30 +1,26 @@
 #include <iostream>
 
 double sq(double x){
-	return x * x;
-}
-
-double repeatTwice(double x, int n){
-	double y;
-	(n % 2 == 0) ? y = sq(x) : (n == 1) ? y = x : y = x * x;
-	return y;
+	x = x * x;
+	return x;
 }
 
 double power(double x, int n){
-	double y;
-	int m;
-	(n % 2 == 0) ? y = repeatTwice(x, n), n = n/2 : (n == 1) ? y = x : y = repeatTwice(x, n), n = n - 1;
-	std::cout << "to test x = " << x << " y = " << y << " and m = " << m << std::endl;
-	(n != 1) ? y = repeatTwice(y, m) : y = y;
-	return y;
+	double y = 0;
+	double z = 0;
+	(n > 0) ? (n == 1) ? y = x : (n % 2 == 1) ? y = sq(x), y = x * y : y = sq(x) : y = x;
+	(n > 0) ? (n == 1) ? n = n : (n % 2 == 1) ? n = n - 3 : n = n / 2 : n = n;
+	(n > 0) ? (n == 1) ? z = y : (n % 2 == 1) ? z = sq(y), z = y * z : z = sq(y) : z = y;
+	(n > 0) ? (n == 1) ? n = n : (n % 2 == 1) ? n = n - 3 : n = n / 2 : n = n;
+	return z;
 }
 
 int main(){
 	std::string cont;
 	while(cont != "no"){
-		double x;
-		int n;
-		double result;
+		double x = 0;
+		int n = 0;
+		double result = 0;
 		std::cout << "Please enter a value for x" << std::endl;
 		std::cin >> x;
 		std::cout << "Please enter the potenz of x" << std::endl;
@@ -34,5 +30,4 @@ int main(){
 		std::cout << "Would you like to enter another number? 'yes' to continue, 'no' to quit" << std::endl; 
 		std::cin >> cont;							// annahme der break variable
 	}
-	
 }
