b0e4f0518629d44cb9fdcd01d4542e1a659c5cd5
Luichang <cbarbret@bucs.fsw.edu>
2016-11-12 17:13:59 +0100

    final or close to final version

--- ostern.cpp
+++ ostern.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <cassert>
 
 template<typename T>
 std::string to_string(const T& value)
@@ -8,25 +9,32 @@ std::string to_string(const T& value)
     std::ostringstream stream;
     stream << value;
     return stream.str();
-}
+} //to string fix
 
-int main(){
-	int a, b, c, d, e, k, p, q, m, n, x, y, z;
-	std::cout << "Geben sie die Jahreszahl ein" << std::endl;
-	std::cin >> y;
+std::string easter(int y) {
+	int a, b, c, d, e, k, p, q, m, n, x, z;
+	int test;
 	std::string Monat;
 	a = y % 19;
 	b = y % 4;
 	c = y % 7;
 	k = y / 100;
-	p = (8 * k + 13)/25;
+	p = (8*k + 13)/25;
 	q = k / 4;
 	m = (15 + k - p - q) % 30;
-	d = (19 * a + m) %30;
+	d = (19*a + m) %30;
 	n = (4 + k - q)% 7;
-	e = (2 * b + 4 * c + 6 * d + n) % 7;
+	e = (2*b + 4*c + 6*d + n) % 7;
 	x = 22 + d + e;
-	(x == 57) ? z = 50 : (x == 56 && d == 28 && a > 10) ? z = 49 : z = x;
-	(z < 32) ? Monat = "Maerz" : Monat = "April", z = z - 31; 
+	(x == 57)? test = 50   :   (x == 56 && d == 28 && a > 10)? test = 49   :   test = x ;
+	(test < 32)? Monat = "Maerz", z=test : z = test - 31, Monat = "April";
 	std::cout << "Ostern war am " << to_string(z) << ". " << Monat;
+}
+
+int main(){
+	int y;
+	std::cout << "Geben sie die Jahreszahl ein" << std::endl;
+	std::cin >> y;
+	assert(y > 1583);
+	easter(y);
 }
