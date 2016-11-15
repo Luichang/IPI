e83ea3096baabd95424db8b6bb790e439afef517
Luichang <cbarbret@bucs.fsw.edu>
2016-11-15 18:41:21 +0100

    added the calculation of the specific angles from a and d

--- sinus.cpp
+++ sinus.cpp
@@ -3,6 +3,15 @@
 #include <cmath>
 #include <iostream>
 
+double reduceX(double x){
+	double twoPi = 2 * M_PI;
+	std::cout << x << std::endl;
+	(x < -M_PI) ? reduceX(x + twoPi)
+		: (x > M_PI) ? reduceX(x - twoPi)
+			: x = x;
+	return x;
+}
+
 double calcAngle(double angle){
 	double result = (2 * M_PI) / 360 * angle;		//diese funktion convertiert die eingabe in bogenmas um dann vom program verwendet werden zu können
 	return result;
@@ -18,6 +27,7 @@ double pump_sin(double sin_third){
 	//double approximation = taylor_sin(sin_third)/ 3;
 	//double approximation = sin_third / 3;
 	sin_third = calcAngle(sin_third);
+	sin_third = taylor_sin(sin_third);
 	double approx = 3 * sin_third - 4 * (sin_third * sin_third * sin_third);
 	return approx;
 }
@@ -35,21 +45,37 @@ double calcSin(double angle){
 	return implemented;
 }
 
+void printSins(){
+	int angles[10] = {5, 10, 20, 45, 90, 135, 180, 225, 270, 315};
+	double print;
+	for(int i = 0; i < 10; i++){
+		print = taylor_sin(angles[i]);
+		std::cout << "Die Annaeherung von taylor_sin fuer " << angles[i] << "	Grad ist " << print << std::endl;
+	}
+	std::cout << std::endl;
+	for(int i = 0; i < 10; i++){
+		print = my_sin(angles[i]);
+		std::cout << "Die Annaeherung von my_sin fuer " << angles[i] << "	Grad ist " << print << std::endl;
+	}
+}
+
 int main(){
+	double /*angle,*/ input, calc, approx, /*approx1, approx2,*/ x;	// variable decleration
 	std::string cont;
+	printSins();
 	while(cont != "no"){								//eingefügt um das program nicht für jeden neuen wert jedes mal neu srarten zu müssen
-		double angle, input, calc, approx, approx1, approx2, x;	// variable decleration
 		std::cout << "This program will take an angle and calculate the Sin value based on a Sin approximation and the implemented Sin function" << std::endl;
 		std::cout << "Please enter an Angle" << std::endl;
 		std::cin >> input;							// annahme des winkels
+		x = reduceX(input);
 		//angle = calcAngle(input);					// winkel zu bogenmas convertieren
-		approx = my_sin(input);
-		approx1 = taylor_sin(input);				// rufen der funktion um sinus anzunähern
-		approx2 = pump_sin(input / 3);					// rufen der funktion um sinus anzunähern
+		approx = my_sin(x);
+		//approx1 = taylor_sin(x);				// rufen der funktion um sinus anzunähern
+		//approx2 = pump_sin(x / 3);				// rufen der funktion um sinus anzunähern
 		calc = calcSin(input);						// rufen der function um sinus genau zu berechnen
 		std::cout << "The answer for the my_sin is:			" << approx << std::endl;
-		std::cout << "The answer for the taylor_sin is:		" << approx1 << std::endl;
-		std::cout << "The answer for the pump_sin is:			" << approx2 << std::endl;
+		//std::cout << "The answer for the taylor_sin is:		" << approx1 << std::endl;
+		//std::cout << "The answer for the pump_sin is:			" << approx2 << std::endl;
 		std::cout << "The implemented version of Sin resulted in:	" << calc << std::endl;
 		std::cout << "Would you like to enter another number? 'yes' to continue, 'no' to quit" << std::endl; 
 		std::cin >> cont;							// annahme der break variable
