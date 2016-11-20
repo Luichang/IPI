edff9ea111eae64eca67e8318e0fca2ac5c9ca18
Luichang <cbarbret@bucs.fsw.edu>
2016-11-20 22:20:26 +0100

    Dont know what to add to complete 4, did not read assignment for 1 but might be done

--- Zettel4/kubikwurzel.cpp
+++ Zettel4/kubikwurzel.cpp
@@ -1,18 +1,16 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
-double sqt(double y){
-	double x = y;
-	while(fabs(x * x - y) > 10){
-		cout << x << endl;
-		x = x + (y - x * x) / (2 * x);
-	}
-}
-
 double cbrt(double y){
-	
+	double x = y;
+	while(fabs(x * x * x - y) > 1e-10){
+		cout << x << endl;
+		x = x/*sollte hier nicht x hoch 2 oder so sein? O.o*/ + (y - x * x * x) / (3 * x * x);
+	}
+	return x;
 }
 
 int main(){
-	
+	cout << cbrt(-27) << endl;
 }
