f93fb7f5b838a992973c3a77493bb1d99d0034a2
Luichang <cbarbret@bucs.fsw.edu>
2016-11-21 17:15:33 +0100

    added comments

--- Zettel4/email.cpp
+++ Zettel4/email.cpp
@@ -1,44 +1,38 @@
 #include <iostream>
 #include <cctype>
 using namespace std;
-/* 
-es darf nur 1 @ vorkommen
-rechts von dem @muss mindestenz 1 punkts sein
-links vom @ muss 1 eröaubtes zeichen vorkommen
-zwischen @ und punkten muss ein zeichen vorkommen
-nach dem punkt muss ein zeichen vorkommen
-*/
-void breakCode(){
-	cout << "You entered an invalid email address.";
-}
+
+void breakCode(){									// #void ist so was schönes. Da muss man NICHTS ausgeben... was ein konzept... versteh ich auch kaum
+	cout << "You entered an invalid email address."; // Funktion eingeführt sollte ich mehr als nur dem user sagen möchte das seine email falsch ist
+}													// #was?? hier musste echt kein return? ich bin erstaunt
 
 bool checkPeriod(string &s){
 	bool Validated = true;
 	int length = s.size();
 	string::size_type position = 0;
-	if(s.find('.') != string::npos){
-		position = s.find('.');
-	}
+	if(s.find('.') != string::npos){	// hier wird der erste und letzte character der "mail" gecheckt ob es ein punkt ist
+		position = s.find('.');			// #da nicht gefordert wurde, dass zwischen jedem punkt mindestens ein erlaubtes zeichen ist
+	}									// #suche ich nicht danach
 	if(position == 0 && position == length - 1){
 		Validated = false;
-	} else {
-		cout << "it works" << endl;
-	}
-	return Validated;
+	} /*else {
+		 cout << "it works" << endl;	diese zeile war ursprünglich zum testen
+	}*/
+	return Validated;	// #bool wird ausgegeben weil die Aufgabe UNBEDINGT ein bool hier möchte
 }
 
 bool checkChar(string &s, int start, int end){
 	char c;
-	char period = '.', underscore = '_', minus = '-';
+	char period = '.', underscore = '_', minus = '-';	// stellt sich heraus einzelne charakter muss man mit '' declarieren und darf sie nicht mit ""
 	bool charIsValid = true;
 	for (int i = start; i < end; ++i){
-		c = s[i];
+		c = s[i];										// arrays sind eine wunderbare erfindung mit der man so schöne Sachen machen kann
 		if(isalnum(c)){
 
 		} else {
-			if(c == period){
+			if(c == period){							// #so halb sinnvolle if's die man bestimmt mit mehr zeit investition entsorgen kann... zu faul
 
-			} else if(c == underscore){
+			} else if(c == underscore){					// #kennt man sicherlich
 
 			} else if(c == minus){
 
@@ -47,44 +41,48 @@ bool checkChar(string &s, int start, int end){
 			}
 		}
 	}
-	return charIsValid;
+	return charIsValid;									// #erneute bool ausgabe weil man ja eh nichts besseres zu tun hat
 }
 
 bool checkAt(string &s){
 	bool dontWork = false;
-	string::size_type position = 0;
-	int length = s.size();
+	string::size_type position = 0;						// faszinierend das der int hier einen etra variabel Typ hat, fragt sich welche sprachen das 
+	int length = s.size();								// mit dem size_type noch verwenden
 	if(s.find('@') != string::npos){
 		position = s.find('@');
 	}
 	if(position != 0){
 		string preAt = s.substr(0, position);
-		string postAt = s.substr(position + 1);
+		string postAt = s.substr(position + 1);			// hier wird + 1 gerechnet, damit man das '@' nicht noch mit im string hat
 		if(checkChar(s, 0, position)){
 			if(checkChar(s, position + 1, length)){
 				if(checkPeriod(postAt)){
-					if(checkPeriod(preAt)){
+					if(checkPeriod(preAt)){				// #viel zu viele if's... weiterhin zu faul das zu verkürzen
 						dontWork = true;
 					}
 				}
 			}
 		}
 	}
-	return dontWork;
+	return dontWork;									// #was soll man denn noch alles returnen?
 }
 
 bool is_email(string &s){
+	bool isEmail = true;								// #hätte hier fast eine ausgabe vergessen... die ist ja auch so wichtig
 	int length = s.length();
 	char c;
 	int atLocation = 0;
 	if(!checkAt(s)){
-		breakCode();
+		isEmail = false;
 	}
+	return isEmail;
 }
 
 int main(){
 	string s;
 	cout << "Please enter an E-Mail you wish to validate:\n";
 	getline(cin, s);
-	is_email(s);
+	if(!is_email(s)){
+		breakCode();
+	}
 }
