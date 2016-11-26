2a5e03c5c840e6d0bf6b0cf501ba058d23d0a39e
Luichang <cbarbret@bucs.fsw.edu>
2016-11-26 23:34:02 +0100

    Added word splitting

--- Random Programs/oVector.cpp
+++ Random Programs/oVector.cpp
@@ -1,4 +1,4 @@
-#include <iostream>	
+/*#include <iostream>	
 #include <vector>
 #include <cmath>
 #include <algorithm>
@@ -18,6 +18,53 @@ vector <string> split_words(string s)
 int main () {
     string s = "Alpha";
 	vector<string> b = split_words(s);
+	for(int i = 0; i < b.size(); ++i){
+		cout << b[i] << endl;
+	}
+    
+    return 0;
+}*/
+#include <iostream>	
+#include <vector>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+vector <string> findeLeerzeichen(string s){
+    vector<string> Leerzeichen;
+    
+    int length = s.size();
+    int pos = s.find(" ");
+    int i;     
+	string pickName;
+    for(int i = 0; i < length; ++i){
+		if(s[i]) == " "){
+			Leerzeichen.push_back(pickName);
+		}else{
+			pickName =+ s[i];
+		}
+	}
+    return Leerzeichen;
+}
+
+vector <string> split_words(string s)
+{
+    vector<string> v1;    
+    v1.push_back(s);
+    
+    //s.find(" ");
+    
+    return v1;
+}
+
+int main () {
+    string s = "Alpha Beta!";
+    string s1, s2;
+    
+    findeLeerzeichen(s);
+	vector<string> b = split_words(s);
+    
 	for(int i = 0; i < b.size(); ++i){
 		cout << b[i] << endl;
 	}
