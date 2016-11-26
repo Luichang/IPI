7ace4489e78ad1d5b451a7d34ffafd2b88f98891
Luichang <cbarbret@bucs.fsw.edu>
2016-11-26 15:05:08 +0100

    added comments

--- Zettel5/perfect_shuffle.cpp
+++ Zettel5/perfect_shuffle.cpp
@@ -10,15 +10,15 @@ void displayVector(vector<int> cards){
 	}
 }
 
-vector<int> init_deck(){
-	vector<int> deck;
-	for(int i = 0; i < 10; ++i){
+vector<int> init_deck(){										// not sure if this is the most self explanatory function ever or what
+	vector<int> deck;											// but it initialises the deck of cards
+	for(int i = 0; i < 52; ++i){
 		deck.push_back(i);
 	}
 	return deck;
 }
 
-bool check_deck(vector<int> cards){
+bool check_deck(vector<int> cards){								// this just wants to check if the cards are back to a non shuffled state
 	if(cards == init_deck()){
 		return true;
 	}else{
@@ -30,8 +30,8 @@ vector<int> shuffle(vector<int> cards, bool out){		// out seems like not the bes
 	vector<int> new_deck;
 	//displayVector(cards);
 	int counter = 0;
-	int maxI = 10;
-	if(out){
+	int maxI = 52;		// at one point I was having huge problems with the vector getting messed up so I just added 
+	if(out){			// this variable to try and stop it from not working
 		for(int i = 0; i < maxI; ++i){
 			if(i % 2 == 0){
 				new_deck.push_back(cards[counter]);
@@ -59,16 +59,16 @@ vector<int> shuffle(vector<int> cards, bool out){		// out seems like not the bes
 int main(){
 	int counter_in = 0;
 	int counter_out = 0;
-	vector<int> shuffled_deck_in = shuffle(init_deck(), false);
-	vector<int> shuffled_deck_out = shuffle(init_deck(), true);
+	vector<int> shuffled_deck_in = shuffle(init_deck(), false);		// the dumbest thing that can happen to you is if you are copying working
+	vector<int> shuffled_deck_out = shuffle(init_deck(), true);		// code and use it for something new, and only change half the variable names
 	while(!check_deck(shuffled_deck_in)){
 		shuffled_deck_in = shuffle(shuffled_deck_in, false);
 		++counter_in;
 		cout << "Counter in is at " << counter_in << endl;
 	}
 	cout << endl;
-	while(!check_deck(shuffled_deck_out)){
-		shuffled_deck_out = shuffle(shuffled_deck_out, true);
+	while(!check_deck(shuffled_deck_out)){							// took me far too long to realize I had the .._in variable name here
+		shuffled_deck_out = shuffle(shuffled_deck_out, true);		// remember kods, always check your variable names before saying your code won't work
 		++counter_out;
 		cout << "Counter in is at " << counter_out << endl;
 	}
