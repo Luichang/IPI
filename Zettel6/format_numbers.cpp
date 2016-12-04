1f4d067c10ddf2c87dd755afc26df1e462cdf9b3
Luichang <cbarbret@bucs.fsw.edu>
2016-12-04 20:13:35 +0100

    added comments and minor fixes

--- Zettel6/format_numbers.cpp
+++ Zettel6/format_numbers.cpp
@@ -1,20 +1,23 @@
 #include <iostream>
 #include <vector>
+#include <string>
 #include <algorithm>
 #include <sstream>
 #include "format_numbers.hpp"
 
 using namespace std;
-
+		
 template<typename T>
-std::string toString(const T& value)
+string toString(const T& value)
 {
     std::ostringstream stream;
-    stream << value;
+    stream << fixed << value;
     return stream.str();
 }
 
-int addPointFive(double in){
+int addPointFive(double in){					// simple lambda function to be reused at a later point
+	if(in < 0)
+		return in - 0.5;
 	return in + 0.5;
 }
 
@@ -26,7 +29,7 @@ vector<int> vector_to_int(vector<double> v){
 
 vector<double> vec_rounded(vector<double> v){
 	vector<double> newDoubleVector(v.size());
-	transform(v.begin(), v.end(), v.begin(), [](double i) {return i * 100;});
+	transform(v.begin(), v.end(), v.begin(), [](double i) {return i * 100;});		// bulky way to have the vectors be transformed into the wanted result
 	transform(v.begin(), v.end(), newDoubleVector.begin(), addPointFive);
 	transform(newDoubleVector.begin(), newDoubleVector.end(), newDoubleVector.begin(), [](double i) {return i / 100;});
 	return newDoubleVector;
@@ -34,20 +37,19 @@ vector<double> vec_rounded(vector<double> v){
 
 string addZeros(string s){
 	int periodPosition;
-	if(s.find('.') != string::npos){
-		periodPosition = s.find('.');
-		if(periodPosition == s.size() - 2){
-			s += "0";
-		}
-	}else{
-		s += ".00";
-	}
+	for(int i = 0; i < 4; ++i){		// OK so since the toString function at first did not add the zeros at the end I was adding zeros here at one point
+		s.pop_back();				// had to use the non implemented way to get it to work on my computer for some reason... thought I had the right
+	}								// version... oh well. this will not clash with the std to_string, seeing as I cleverly changed the name 
 	return s;
 }
 
 string addApostophe(string s){
-	if(s.size() > 6){
-		s.insert(s.size() - 6, "'");
+	int position = s.find('.');
+	while(position > 3){
+		if(s[position - 4] != '-'){						// just going through each number and making sure that the aposophe is
+			s.insert(position - 3, "'");				// every 3 numbers (for numbers like thousands)
+		}
+		position = position - 3;
 	}
 	return s;
 }
@@ -72,13 +74,13 @@ vector<string> format_numbers(vector<double> v){
 }
 
 int main(){
-	vector<double> madeOfDoubeles = {12485.3, 19.5, 2.28754, 4.8654};
+	/*vector<double> madeOfDoubeles = {12485.3, 19.5, 2.28754, 4.8654};
 	vector<int> madeOfInts = vector_to_int(madeOfDoubeles);
-	/*for(auto i : madeOfInts){
+	for(auto i : madeOfInts){												// comenting out the previous assignment parts and just leaving the final part
 		cout << double_to_string(i) << endl;
-	}*/
+	}
 	vector<double> madeOfNewDoubles = vec_rounded(madeOfDoubeles);
-	/*for(auto i : madeOfNewDoubles){
+	for(auto i : madeOfNewDoubles){
 		cout << double_to_string(i) << endl;
 	}*/
 	vector<string> madeOfStrings = format_numbers(numbers);
--- Zettel6/sort_versions.cpp
+++ Zettel6/sort_versions.cpp
@@ -6,18 +6,26 @@
 
 using namespace std;
 
+template<typename T>
+void printVectors(const vector<T> printV){		// starting to get the hang of templates, trying to remember the following:
+	for(auto i:printV){							// you add a typename variable, this can be anything like an int, or string etc
+		cout << i << endl;						// then as the variable to receive just use const, and the ambiguous variable
+	}											// pretend like it's a normal function
+	cout << endl;
+}
+
 vector<int> split_version(string version){
 	int periodPosition = 0;
 	string partial;
 	vector<int> splitedVersion;
 	for(int i = 0; i < version.size(); ++i){
-		if(version[i] == '.'){
-			partial = version.substr(periodPosition, i);
-			periodPosition = i + 1;
+		if(version[i] == '.'){									// this looks for reach period and adds the "substring", from between the last period,
+			partial = version.substr(periodPosition, i);		// with out the period it self, to a new vector
+			periodPosition = i + 1;								// the position of the period will be marked for the next substring
 			splitedVersion.push_back(atoi(partial.c_str()));
 		}
 	}
-	partial = version.substr(periodPosition, version.size());
+	partial = version.substr(periodPosition, version.size());	// adds the last bit that does not have a following period
 	splitedVersion.push_back(atoi(partial.c_str()));
 	return splitedVersion;
 }
@@ -25,8 +33,14 @@ vector<int> split_version(string version){
 bool version_less(string v1, string v2){
 	vector<int> version1 = split_version(v1);
 	vector<int> version2 = split_version(v2);
+	while(version1.size() < version2.size()){			// ugly way to ensure that vectors to be compared are the same length to properly compare them
+		version1.push_back(0);
+	}
+	while(version1.size() > version2.size()){
+		version2.push_back(0);
+	}
 	for(int i = 0; i < version1.size(); ++i){
-		if(version1[i] < version2[i])
+		if(version1[i] < version2[i])					// simple check to see which is larger by comparing each sub part of the vector
 			return true;
 		if(version1[i] > version2[i])
 			return false;
@@ -35,15 +49,18 @@ bool version_less(string v1, string v2){
 }
 
 int main(){
-	string version1 = "4.31.53";
-	string version2 = "4.31.5";
-	vector<int> sVersion = split_version(version1);
-	for(auto i:sVersion){
-		cout << i << endl;
-	}
-	cout << version_less(version1, version2);
+	/*string version1 = "4.31";
+	string version2 = "4.31.1";
+	vector<int> sVersion1 = split_version(version1);			// just commenting this out because not relevant in last parts of the assignment
+	vector<int> sVersion2 = split_version(version2);
+	printVectors(sVersion1);
+	printVectors(sVersion2);
+	vector<string> combined;
+	combined.push_back(version1);
+	combined.push_back(version2);
+	printVectors(combined);
+	sort(combined.begin(), combined.end(), version_less);
+	printVectors(combined);*/
 	sort(versions.begin(), versions.end(), version_less);
-	for(auto i:versions){
-		cout << i << endl;
-	}
+	printVectors(versions);
 }
